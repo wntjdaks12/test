6c36458ee3424732c2cdb089885b683420bd484b
wntjdaks12 <71547403+wntjdaks12@users.noreply.github.com>
2020-09-20 01:25:11 +0900

    Add files via upload

---
+++ calcul.js
@@ -0,0 +1,7 @@
+var num1 = 6;
+var num2 = 2;
+
+console.log('더하기 : ' + (num1 + num2));
+console.log('빼기 : ' + (num1 - num2));
+console.log('곱하기 : ' + (num1 * num2));
+console.log('나누기 : ' + (num1 / num2));
